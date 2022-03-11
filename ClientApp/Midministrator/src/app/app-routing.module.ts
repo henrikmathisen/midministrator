518341653f3ca2715b292ef31ff7526ab251f112
Henrik Mathisen <henrik.mathisen@medhelp.se>
2022-03-11 10:40:18 +0100

    removed auth guard

--- ClientApp/Midministrator/src/app/app-routing.module.ts
+++ ClientApp/Midministrator/src/app/app-routing.module.ts
@@ -5,36 +5,45 @@ import { HttpClientModule } from '@angular/common/http';
 import { SpinnerService } from './services/spinner.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/clients', pathMatch: 'full', canLoad: [AuthGuardService], canActivate: [AuthGuardService] },
+  { path: '', redirectTo: '/clients', pathMatch: 'full', 
+    // canLoad: [AuthGuardService], canActivate: [AuthGuardService] 
+  },
   { path: 'clients', 
     loadChildren: () => import ('./client/client.module').then(mod => mod.ClientModule)
   },
   {
-    path: 'applications', canLoad: [AuthGuardService], canActivate: [AuthGuardService],
+    path: 'applications', 
+    // canLoad: [AuthGuardService], canActivate: [AuthGuardService],
       loadChildren: () => import('./application/application.module').then(mod => mod.ApplicationModule)
   },
   {
-    path: 'roles', canLoad: [AuthGuardService], canActivate: [AuthGuardService],
+    path: 'roles', 
+    //canLoad: [AuthGuardService], canActivate: [AuthGuardService],
     loadChildren: () => import('./role/role.module').then(mod => mod.RoleModule)
   },
   {
-    path: 'users', canLoad: [AuthGuardService], canActivate: [AuthGuardService],
+    path: 'users', 
+    //canLoad: [AuthGuardService], canActivate: [AuthGuardService],
     loadChildren: () => import('./user/user.module').then(mod => mod.UserModule)
   },
   {
-    path: 'groups', canLoad: [AuthGuardService], canActivate: [AuthGuardService],
+    path: 'groups', 
+    //canLoad: [AuthGuardService], canActivate: [AuthGuardService],
     loadChildren: () => import('./group/group.module').then(mod => mod.GroupModule)
   },
   {
-    path: 'grants', canLoad: [AuthGuardService], canActivate: [AuthGuardService],
+    path: 'grants', 
+    //canLoad: [AuthGuardService], canActivate: [AuthGuardService],
     loadChildren: () => import('./grant/grant.module').then(mod => mod.GrantModule)
   },
   {
-    path: 'apiresources', canLoad: [AuthGuardService], canActivate: [AuthGuardService],
+    path: 'apiresources', 
+    //canLoad: [AuthGuardService], canActivate: [AuthGuardService],
     loadChildren: () => import('./api-resource/api-resource.module').then(mod => mod.ApiResourceModule)
   },
   {
-    path: 'identityresources', canLoad: [AuthGuardService], canActivate: [AuthGuardService],
+    path: 'identityresources', 
+    //canLoad: [AuthGuardService], canActivate: [AuthGuardService],
     loadChildren: () => import('./identity-resource/identity-resource.module').then(mod => mod.IdentityResourceModule)
   }
 ];
